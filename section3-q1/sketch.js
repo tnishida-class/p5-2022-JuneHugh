0b6396a60efabb57800ccf552612e624f86ce22c
JuneHugh <jh72478@uga.edu>
2022-10-25 12:18:00 +0900

    Section 4-1 Assignments
    Line graph: I added 10 circles at (i * dx + 20, score value). Then connect lines at the exact same places. This creates a line graph!
    
    Bar Graph: I made the max and min values red and blue respectively. Then I added a green average line and then added the text above the average line.

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -2,9 +2,13 @@
 let count;
 let cycle;
 let size = 25
+// this boolean variable detects if the heart is
+// in beating motion. This makes it so that the heart can fully go back to regular size.
 let beating = true;
 const heartRate = .01;
 const heartRateMax = 2.5;
+
+// trying to make the heart jump
 const gravity = .025;
 const maxGravity = 15;
 
@@ -82,18 +86,9 @@ function draw(){
 
   }
 
-  /*if (keyCode == LEFT_ARROW) {
-    x -= 1;
-  } else if (keyCode == RIGHT_ARROW) {
-    x += 1;
-  } else if (keyCode == SPACE) {
-    y -= vy;
-    vy += gravity;
-    vy = constrain(vy, gravity, maxGravity);
-  } else if (keyCode == DOWN_ARROW) {
-    y += 1;
-  } */
-
+  // if the arrow keys are pressed, the circle will move in the
+  // cooresponding direction! Eventually I will try to make the cicle JUMP
+  // but I'm still not quite sure how to do that so I'll ask you guys.
   if (keyIsPressed && keyCode == LEFT_ARROW) {
     x -= 1;
   } else if (keyIsPressed && keyCode == RIGHT_ARROW) {
@@ -109,10 +104,7 @@ function draw(){
     vy = constrain(vy, gravity, maxGravity);*/
     y -= 1;
   }
-  //y += 1;
 
-
-  
 
   // if circle goes out of bounds, or out of screen / canvas, it will reappear on the other side!
   x = (x + width) % width;
