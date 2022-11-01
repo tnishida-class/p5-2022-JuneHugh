8d429ad46021b5ec05a6a0c74fc51af9b1c7eeb2
JuneHugh <jh72478@uga.edu>
2022-11-02 01:15:51 +0900

    Assignments for 11/1/2022
    I messed around with some of the lines of code in section 5-1 to understand what exactly was happening. Once I understood what was happening, I switched the ellipses that were being drawn to stars and then used the given formula to draw 12 stars in a perfect circle. Then I colored the canvas blue to make it look like the EU flag.
    
    In section5-2 I changed the star to a polygon with 9 points and sides, however I made it so that the user can manipulate the number of points / sides freely by making it an argument.
    
    In section 5-q1, I didn't know what to do because there were no examples shows to me... Anyways, I made the rectangle red and then I added more arguments to the function balloon(). I added the color of the rectangle or balloon, the desired X coordinate, and the desired Y coordinate.

--- section5-2/sketch.js
+++ section5-2/sketch.js
@@ -6,7 +6,7 @@ function setup(){
   fill(0);
   crossmark(10, 10, 90, 90);
   ngmark(150, 50, 80);
-  star(250, 50, 40);
+  regularPolygon(9, 250, 50, 40);
 }
 
 function crossmark(x1, y1, x2, y2){
@@ -15,19 +15,19 @@ function crossmark(x1, y1, x2, y2){
 }
 
 function ngmark(cx, cy, r){
-  push();
+  push(); // kind of makes a new branch in git and you can do stuff here without affecitng the other main branch
   noFill();
   strokeWeight(r * 0.1);
   let d = sqrt(r * r / 8);
   ellipse(cx, cy, r);
   line(cx - d, cy - d, cx + d, cy + d);
-  pop();
+  pop(); // switches back to the main branch so that the settings are remembered.
 }
 
-function star(cx, cy, r){
+function regularPolygon(n, cx, cy, r){
   beginShape();
-  for(var i = 0; i < 5; i++){
-    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+  for(var i = 0; i < n; i++){
+    let theta = TWO_PI * i / n;
     let x = cx + cos(theta) * r;
     let y = cy + sin(theta) * r;
     vertex(x,y);
--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,15 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 'red', width / 2, height / 2);
 }
 
-function balloon(t){
+function balloon(t, color, positionX, positionY){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  let p = 10;
+  fill(color);
+  rect(positionX, positionY, w + p * 2, h + p * 2);
   fill(255);
-  text(t, p, h + p);
+  text(t, positionX, (positionY + h + p * 2 / 2));
 }
